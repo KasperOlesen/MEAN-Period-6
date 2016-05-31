ab690455b6a43bca9e05bdc0ffe898ba4a301123
KasperOlesen <KasperAolesen@gmail.com>
2016-05-31 15:48:27 +0200

    Added better examples to AppCORS.js

--- AppCORS.js
+++ AppCORS.js
@@ -22,3 +22,35 @@ angular.module('starter.services', [])
         };
     })
 
+
+//Example of simple REST get request in Ionic
+exampleApp.controller('ExampleController', function($scope, $http) {
+
+    $scope.getData = function() {
+        $http.get("http://localhost/example.json", { params: { "key1": "value1", "key2": "value2" } })
+            .success(function(data) {
+                $scope.firstname = data.firstname;
+                $scope.lastname = data.lastname;
+            })
+            .error(function(data) {
+                alert("ERROR");
+            });
+    }
+
+});
+
+
+//Example of solving CORS problems
+//To solve CORS problems you need to setup a proxy server that sends our request to the the real api.
+//To do this setup ionic.project like this.
+
+{
+    "name": "proxy-example",
+    "app_id": "",
+    "proxies": [
+    {
+        "path": "/api",
+        "proxyUrl": "http://cors.api.com/api"
+    }
+]
+}
